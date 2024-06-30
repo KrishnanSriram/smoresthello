c41ef3456173da104bd1ccfde1c763188f8ee457
Krishnan <krishnan.sriram@hotmail.com>
2024-06-30 17:49:53 -0400

    Code to make things work in docker. Abstraction does not help much

--- app.py
+++ app.py
@@ -12,35 +12,19 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)  # Redirect logs to standard output
     ]
 )
-class Main:
-    def __init__(self):
-        self.app = Flask(__name__)
-        logging.info("Created app")
-        self.init_app()
-        self.api = Api(self.app)
 
-    def init_app(self):
-        self.app.config['PROPAGATE_EXCEPTIONS'] = True
-        self.app.config['API_TITLE'] = 'Flask-Smorest API'
-        self.app.config['API_VERSION'] = 'v1'
-        self.app.config['OPENAPI_VERSION'] = '3.0.3'
-        self.app.config['OPENAPI_URL_PREFIX'] = '/docs'
-        self.app.config['OPENAPI_SWAGGER_UI_PATH'] = '/'
-        self.app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
-        logging.info("Initialized app")
 
-    def add_blueprint(self, bp):
-        self.api.register_blueprint(bp)
-        logging.info("Added BP")
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['API_TITLE'] = 'Flask-Smorest API'
+app.config['API_VERSION'] = 'v1'
+app.config['OPENAPI_VERSION'] = '3.0.3'
+app.config['OPENAPI_URL_PREFIX'] = '/docs'
+app.config['OPENAPI_SWAGGER_UI_PATH'] = '/'
+app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
 
-    def run(self):
-        logging.info("Running app.....")
-        self.app.run(debug=True, port=5000, host="127.0.0.1")
-        logging.info("App Stoppped!!")
-
-# if __name__ == "__main__":
-# MAIN SECTION
-main = Main()
-main.add_blueprint(products_blp)
+api = Api(app)
+api.register_blueprint(products_blp)
 init_products()
-main.run()
+
+app.run(debug=True, port=5000, host="127.0.0.1")
